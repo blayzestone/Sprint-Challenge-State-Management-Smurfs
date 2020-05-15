a57fd0ca1763b498e16fdbb5f4a000fcf4a20891
Blayze <blayzestone@protonmail.com>
2020-05-15 10:54:30 -0700

    feat: title of App

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -17,6 +17,7 @@ const App = () => {
 
     return (
       <div className="App">
+        <h1>Smurf Village</h1>
         { isFetching && <div>Fetching Smurfs...</div> }
         <SmurfList />
         <CreateSmurfForm />
